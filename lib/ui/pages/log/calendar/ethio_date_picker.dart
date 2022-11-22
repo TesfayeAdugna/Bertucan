80ce2eb3505dfeb3bed9786fb60e601cd7603b8b
Tesfaye <adugnatesfaye888@gmail.com>
2022-11-22 12:52:43 +0300

    [tesfaye] implemented the screen for select day using Et calendar

--- lib/ui/pages/log/calendar/ethio_date_picker.dart
+++ lib/ui/pages/log/calendar/ethio_date_picker.dart
@@ -4,38 +4,6 @@ import 'package:abushakir/abushakir.dart';
 import 'blocs/blocs.dart';
 import 'size_config.dart';
 
-const List<String> _dayNumbers = [
-  "፩",
-  "፪",
-  "፫",
-  "፬",
-  "፭",
-  "፮",
-  "፯",
-  "፰",
-  "፱",
-  "፲",
-  "፲፩",
-  "፲፪",
-  "፲፫",
-  "፲፬",
-  "፲፭",
-  "፲፮",
-  "፲፯",
-  "፲፰",
-  "፲፱",
-  "፳",
-  "፳፩",
-  "፳፪",
-  "፳፫",
-  "፳፬",
-  "፳፭",
-  "፳፮",
-  "፳፯",
-  "፳፰",
-  "፳፱",
-  "፴",
-];
 class EthioDatePicker extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -59,9 +27,9 @@ class MyCalendar extends StatelessWidget {
   static double widthMultiplier = SizeConfig.widthMultiplier?.toDouble() ?? 1;
   static double heightMultiplier = SizeConfig.heightMultiplier?.toDouble() ?? 1;
 
-  EtDatetime _today = EtDatetime.now();
+  ETC selected_date = ETC.today();
 
-  List<Text> _days = [
+  final List<Text> _days = [
     Text(
       "ሰ",
       style: TextStyle(
@@ -87,19 +55,6 @@ class MyCalendar extends StatelessWidget {
             fontSize: 2.08335 * textMultiplier, fontWeight: FontWeight.bold)),
   ];
 
-  String clockDivision(int hour) {
-    if (hour >= 0 && hour <= 4)
-      return "ከሌሊቱ";
-    else if (hour >= 5 && hour <= 9)
-      return "ከጠዋቱ";
-    else if (hour >= 10 && hour <= 12)
-      return "ከረፋዱ";
-    else if (hour >= 13 && hour <= 16)
-      return "ከሰአት";
-    else
-      return "ከምሽቱ";
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -108,66 +63,57 @@ class MyCalendar extends StatelessWidget {
         create: (BuildContext context) =>
             CalendarBloc(currentMoment: ETC.today()),
         child: Scaffold(
-          body: Container(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                IconButton(
-                    onPressed: () => Navigator.pop(context),
-                    icon: Icon(Icons.close)),
-                Expanded(child: BlocBuilder<CalendarBloc, CalendarState>(
-                  builder: (context, state) {
-                    final month = state.moment;
-                    return Column(
-                      children: <Widget>[
-                        Padding(
-                            padding: EdgeInsets.symmetric(vertical: 5),
-                            child: _nameAndActions(context, month)),
-                        _dayNames(),
-                        Expanded(
-                            child: GestureDetector(
-                                onPanEnd: (e) {
-                                  if (e.velocity.pixelsPerSecond.dx < 0) {
-                                    BlocProvider.of<CalendarBloc>(context)
-                                        .add(NextMonthCalendar(month));
-                                  } else if (e.velocity.pixelsPerSecond.dx >
-                                      0) {
-                                    BlocProvider.of<CalendarBloc>(context)
-                                        .add(PrevMonthCalendar(month));
-                                  }
-                                },
-                                child: ListView(
-                                  children: [
-                                    _daysGridList(context, month),
-                                    Divider(),
-                                    ListTile(
-                                        leading: CircleAvatar(
-                                            backgroundColor: Colors.red),
-                                        title: Text("በ ወር አበባ ላይ")),
-                                    ListTile(
-                                        leading: Container(
-                                          height: 40,
-                                          width: 40,
-                                          decoration: BoxDecoration(
-                                              border: Border.all(
-                                                  color: Colors.orange),
-                                              borderRadius: BorderRadius.all(
-                                                  Radius.circular(20))),
-                                        ),
-                                        title: Text("ጥሩ የ እርዝግዝና እድል")),
-                                    ListTile(
-                                        leading: CircleAvatar(
-                                            backgroundColor: Colors.orange),
-                                        title: Text("በጣም ከፍተኛ የ እርግዝና እድል")),
-                                  ],
-                                ))),
-                      ],
-                    );
-                  },
-                )),
-              ],
-            ),
+          body: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(child: BlocBuilder<CalendarBloc, CalendarState>(
+                builder: (context, state) {
+                  final month = state.moment;
+                  return Column(
+                    children: <Widget>[
+                      _nameAndActions(context, month),
+                      _dayNames(),
+                      Expanded(
+                          child: GestureDetector(
+                              onPanEnd: (e) {
+                                if (e.velocity.pixelsPerSecond.dx < 0) {
+                                  BlocProvider.of<CalendarBloc>(context)
+                                      .add(NextMonthCalendar(month));
+                                } else if (e.velocity.pixelsPerSecond.dx > 0) {
+                                  BlocProvider.of<CalendarBloc>(context)
+                                      .add(PrevMonthCalendar(month));
+                                }
+                              },
+                              child: ListView(
+                                children: [
+                                  _daysGridList(context, month),
+                                ],
+                              ))),
+                    ],
+                  );
+                },
+              )),
+              Padding(
+                padding: EdgeInsets.only(bottom: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    SizedBox(),
+                    SizedBox(),
+                    TextButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: Text("CANCEL",
+                            style: TextStyle(color: Colors.orange))),
+                    TextButton(
+                        onPressed: () {},
+                        child:
+                            Text("OK", style: TextStyle(color: Colors.orange)))
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
@@ -175,35 +121,67 @@ class MyCalendar extends StatelessWidget {
   }
 
   Widget _nameAndActions(BuildContext context, ETC a) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Row(
-          children: [
-            IconButton(
-              icon: Icon(Icons.chevron_left),
-              color: Colors.black,
-              iconSize: 6.94 * imageSizeMultiplier,
-              onPressed: () {
-                BlocProvider.of<CalendarBloc>(context)
-                    .add(PrevMonthCalendar(a));
-              },
-            ),
-            Text(
-              "${a.monthName}, ${a.year}",
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 3.177 * textMultiplier,
-                  color: Colors.black),
-            ),
-          ],
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.symmetric(
+              horizontal: 4.63 * widthMultiplier,
+              vertical: 1.838 * heightMultiplier),
+          color: Colors.orange,
+          height: 15 * heightMultiplier,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "ቀን ይምረጡ",
+                style: TextStyle(color: Colors.white),
+              ),
+              Text(
+                "${(selected_date.monthDays(weekDayName: true).toList())[selected_date.day-1][3]}, ${selected_date.monthName} ${selected_date.day}",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 3.177 * textMultiplier,
+                    color: Colors.white),
+              ),
+            ],
+          ),
         ),
-        IconButton(
-          icon: Icon(Icons.chevron_right),
-          iconSize: 6.94 * imageSizeMultiplier,
-          onPressed: () {
-            BlocProvider.of<CalendarBloc>(context).add(NextMonthCalendar(a));
-          },
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 4.63 * widthMultiplier),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                "${a.monthName} ${a.year}",
+                style: TextStyle(
+                    fontSize: 2.177 * textMultiplier,
+                    color: Colors.black),
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.chevron_left),
+                    color: Colors.black,
+                    iconSize: 6.94 * imageSizeMultiplier,
+                    onPressed: () {
+                      BlocProvider.of<CalendarBloc>(context)
+                          .add(PrevMonthCalendar(a));
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.chevron_right),
+                    iconSize: 6.94 * imageSizeMultiplier,
+                    onPressed: () {
+                      BlocProvider.of<CalendarBloc>(context)
+                          .add(NextMonthCalendar(a));
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ],
     );
@@ -223,90 +201,70 @@ class MyCalendar extends StatelessWidget {
   }
 
   Widget _daysGridList(BuildContext context, ETC a) {
-    int today;
     int lengthOfMonthdays = a.monthDays().toList().length;
     int valueAtIndex3 = a.monthDays().toList()[0][3];
     return GridView.count(
-      shrinkWrap: true,
-      primary: false,
-      padding: EdgeInsets.symmetric(
-          horizontal: 0.694 * widthMultiplier,
-          vertical: 0.3676 * heightMultiplier),
-      crossAxisCount: 7,
-      children: List.generate(lengthOfMonthdays + valueAtIndex3, (index) {
-        if (a.monthDays().toList()[0][3] > 0 &&
-            index < a.monthDays().toList()[0][3]) {
-          // NULL printer
-          return GestureDetector (
-        
-            child:Container(
-              alignment: Alignment.center,
-              height: 1.225 * heightMultiplier,
-              width: 2.315 * widthMultiplier,
-              child: Text(
-                "",
-                style: TextStyle(color: Colors.black),
-              ),
-              decoration: BoxDecoration(
-                color: Colors.grey[100],
-                borderRadius: BorderRadius.circular(60),
-              ),
-            ));
-        } else {
-          // mark if currentday == today
-          List monthDaysList = a.monthDays().toList();
-          if (monthDaysList[(index - monthDaysList[0][3]).toInt()][0] ==
-                  EtDatetime.now().year &&
-              monthDaysList[(index - monthDaysList[0][3]).toInt()][1] ==
-                  EtDatetime.now().month &&
-              monthDaysList[(index - monthDaysList[0][3]).toInt()][2] ==
-                  EtDatetime.now().day) {
-            return GestureDetector (
-              onTap: () => print("hello there"),
-              child: Container(
+        shrinkWrap: true,
+        primary: false,
+        padding: EdgeInsets.symmetric(
+            horizontal: 0.694 * widthMultiplier,
+            vertical: 0.3676 * heightMultiplier),
+        crossAxisCount: 7,
+        children: List.generate(
+          lengthOfMonthdays + valueAtIndex3,
+          (index) {
+            if (a.monthDays().toList()[0][3] > 0 &&
+                index < a.monthDays().toList()[0][3]) {
+              // NULL printer
+              return GestureDetector(
+                  child: Container(
                 alignment: Alignment.center,
                 height: 1.225 * heightMultiplier,
                 width: 2.315 * widthMultiplier,
-                child: Text(
-                  "${a.monthDays().toList()[(index - a.monthDays().toList()[0][3]).toInt()][2]}",
-                  style: TextStyle(color: Colors.black),
-                ),
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(179, 116, 71, 183),
-                  borderRadius: BorderRadius.circular(55),
-                ),
-              
-            ));
-          }
-          return GestureDetector(
-            onTap: () => print("whatever"),
-            child: Container(
-              alignment: Alignment.center,
-              height: 1.225 * heightMultiplier,
-              width: 2.315 * widthMultiplier,
-              child: Text(
-                "${a.monthDays().toList()[(index - a.monthDays().toList()[0][3]).toInt()][2]}",
-                style: TextStyle(color: Colors.black),
-              ),
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-                borderRadius: BorderRadius.circular(80),
-              ),
-            ));
-        }
-      },
-    ));
-  }
-
-  Widget _myDate(EtDatetime dt) {
-    return Row(
-      children: <Widget>[
-        Text(
-          "${dt.monthGeez} ${_dayNumbers[dt.day - 1]}, ${ConvertToEthiopic(dt.year)}",
-          style: TextStyle(
-              fontSize: 3.0 * textMultiplier, fontWeight: FontWeight.w500),
-        ),
-      ],
-    );
+                child: const Text(""),
+              ));
+            } else {
+              // mark if currentday == today
+              List monthDaysList = a.monthDays().toList();
+              if (monthDaysList[(index - monthDaysList[0][3]).toInt()][0] ==
+                      EtDatetime.now().year &&
+                  monthDaysList[(index - monthDaysList[0][3]).toInt()][1] ==
+                      EtDatetime.now().month &&
+                  monthDaysList[(index - monthDaysList[0][3]).toInt()][2] ==
+                      EtDatetime.now().day) {
+                return GestureDetector(
+                    onTap: () => print("hello there"),
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 1.225 * heightMultiplier,
+                      width: 2.315 * widthMultiplier,
+                      child: Text(
+                        "${a.monthDays().toList()[(index - a.monthDays().toList()[0][3]).toInt()][2]}",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(55),
+                      ),
+                    ));
+              }
+              return GestureDetector(
+                  onTap: () => print("whatever"),
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 1.225 * heightMultiplier,
+                    width: 2.315 * widthMultiplier,
+                    child: Text(
+                      "${a.monthDays().toList()[(index - a.monthDays().toList()[0][3]).toInt()][2]}",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(80),
+                    ),
+                  ));
+            }
+          },
+        ));
   }
 }
